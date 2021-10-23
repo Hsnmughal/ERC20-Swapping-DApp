fe064f53e0e662971bab858617df1127447301db
Hsnmughal <hassanmughal12386@gmail.com>
2021-10-23 18:48:34 +0500

    Functions Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,51 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Web3 from 'web3';
 // import logo from '../logo.png';
 import './App.css';
-import detectEthereumProvider from '@metamask/detect-provider';
 
 const App = () => {
 
+  const [account, setAccount] = useState('');
+  const [accountBal, setAccountBal] = useState('');
+
+  useEffect(() => {
+    loadWeb3();
+    loadBlockChainData();
+    console.log(window.web3)
+  }, [])
+
+  const loadBlockChainData = async () => {
+    // Getting WEB3
+    const web3 = window.web3;
+
+    // FETCHING ACCOUNT
+    const account = await web3.eth.getAccounts();
+    setAccount(account[0])
+    console.log(account[0])
+
+    // FETCHING BALANCE
+    const accountBalance = await web3.eth.getBalance(account[0]);
+    setAccountBal(accountBalance);
+    console.log(accountBalance);
+
+  }
 
   const loadWeb3 = async () => {
-    // This function detects most providers injected at window.ethereum
-
-    const provider = await detectEthereumProvider();
-
-    if (provider) {
-      // From now on, this should always be true:
-      // provider === window.ethereum
-      startApp(provider); // initialize your app
-    } else {
-      console.log('Please install MetaMask!');
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
   }
 
   return (
     <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dapp University
-        </a>
-      </nav>
+      
       <div className="container-fluid mt-5">
         <div className="row">
           <main role="main" className="col-lg-12 d-flex text-center">
